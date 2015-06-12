e71ccd4d285b2b1d8177809818f197f70ff4e072
Christian Ospina <chrisdoc7@hotmail.com>
2015-06-12 16:51:48 -0500

    Cambios antes del primer break

--- gulpfile.js
+++ gulpfile.js
@@ -12,24 +12,66 @@ var nib = require('nib')
 var minify = require('gulp-minify-css')
 var uglify = require('gulp-uglify')
 
+var watchify = require('watchify')
+var assign = require('lodash.assign')
+var livereload = require('gulp-livereload')
+
+var opts = {
+    entries: './lib/app.js', //punto de entrada js
+    transform: [ babelify, jadeify] //transformaciones
+  }
+
+opts = assign({}, watchify.args, opts)
+
 gulp.task('build', ['styl', 'js'])
 
 gulp.task('js', function() {
-  return browserify({
-    entries: './lib/app.js', //punto de entrada js
-    transform: [ babelify, jadeify] //transformaciones
-  })
-  .bundle()
-  .pipe(source('app.js')) // archivo destino
-  .pipe(buffer())
-  .pipe(uglify())
-  .pipe(gulp.dest('./public/')) // en dónde va a estar el archivo destino
+  return generateBundle(browserify(opts))
 })
 
 gulp.task('styl', function() {
+  return styl()
+})
+
+gulp.task('styl:livereload', function() {
+  return styl().pipe(livereload({ start: true }))
+})
+
+gulp.task('styl:watch', function() {
+  return gulp.watch(['./lib/app.styl', './lib/**/*.styl'], ['styl:livereload'])
+})
+
+gulp.task('js:watch', function() {
+  var w = watchify(browserify(opts))
+
+  w.on('update', function(file) {
+    //lógica de rebuild
+    console.log('file modified, rebuilding: ', file)
+
+    var bdle = generateBundle(w).pipe(livereload())
+    console.log('rebuild finished')
+    return bdle
+  })
+
+  // livereload es un Singleton
+  return generateBundle(w).pipe(livereload({ start: true }))
+})
+
+gulp.task('watch', ['styl:watch', 'js:watch'])
+
+function styl() {
   return gulp.src('./lib/app.styl') // entry point de styl
     .pipe(stylus({ use: nib() })) //inicializo stylus con nib como plugin
     .pipe(concat('app.css'))
-    .pipe(minify())
+    //.pipe(minify())
     .pipe(gulp.dest('./public/css'))
-})
+}
+
+function generateBundle(b) {
+  return b
+  .bundle()
+  .pipe(source('app.js')) // archivo destino
+  .pipe(buffer())
+  //.pipe(uglify())
+  .pipe(gulp.dest('./public/')) // en dónde va a estar el archivo destino
+}
